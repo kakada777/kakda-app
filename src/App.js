e797f593a91b3368767237d3e0c0810a86931254
macbookpro <kakakda772003@gmail.com>
2024-11-20 08:54:50 +0700

    h

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
-import Home from './pages/Home';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Read2 from './pages/Read2';
 import About from './pages/About';
 import Navbar from './components/Navbar';
@@ -10,15 +11,15 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter basename='/subdirectory'>
+    <BrowserRouter basename="/kakda-app">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/read/:id' element={<Read2 />} />
-        <Route path='/about-us' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/shoes' element={<Shoes />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/read/:id" element={<Read2 />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shoes" element={<Shoes />} />
       </Routes>
       <Footer />
     </BrowserRouter>
